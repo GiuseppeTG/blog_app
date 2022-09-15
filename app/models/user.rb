309e3ad7314131529ebdf7fb53aebb4033348774
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-09-14 20:05:33 -0500

    Update readme and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def show_recent_posts
     posts.order('created_at DESC').limit(3)
