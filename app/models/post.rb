0e3685f6af0c3e3c85be411e9512ea313a2c2c7f
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-09-20 15:30:45 -0500

    first draft of users index view

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,16 +7,20 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_save :update_post_counter
+
   after_initialize do |post|
     post.likes_counter = 0
     post.comments_counter = 0
   end
 
-  def update_posts_counter
-    author.increment!(:posts_counter)
-  end
-
   def show_recent_comments
     comments.order(updated_at: :desc).first(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
