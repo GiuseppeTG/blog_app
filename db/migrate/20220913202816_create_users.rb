681ac269515d7c62f5fba8d7f66c639d367a15bb
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-09-28 13:10:38 -0500

    Add user role column

--- db/migrate/20220913202816_create_users.rb
+++ db/migrate/20220913202816_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :photo, null: false, default: 'https://www.kindpng.com/picc/m/22-223965_no-profile-picture-icon-circle-member-icon-png.png'
       t.text :bio
       t.integer :posts_counter, null: false, default: 0
+      t.string :role, null: false, default: 'user'
 
       t.timestamps
     end
